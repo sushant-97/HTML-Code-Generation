774b166342289f664a2898aa4e1051fe4b4846f5
sushant-97 <65329646+sushant-97@users.noreply.github.com>
2023-12-25 12:05:20 +0530

    Update llm_interface.py

--- app/llm_interface.py
+++ app/llm_interface.py
@@ -1,19 +1,24 @@
-# Load model directly
+# Description: Interface for the llama2-fine-tuned-jawerty_html_dataset model
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import GenerationConfig
+from types import SimpleNamespace
 
-model_id = "<username>/llama2-fine-tuned-dolly-15k"
+model_id = "llama2-fine-tuned-jawerty_html_dataset"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id)
 
-# Generate text
-def query_llm(prompt):
-    # Encode prompt
-    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+# Generate code
+def generate(prompt, model_id, model, tokenizer):
+    gen_config = GenerationConfig.from_pretrained(model_id)
+    test_config = SimpleNamespace(
+        max_new_tokens=256,
+        gen_config=gen_config)
 
-    # Generate text
-    output = model.generate(input_ids, do_sample=True, max_length=1000, top_p=0.95, top_k=60)
-
-    # Decode output
-    output = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return output
+    tokenized_prompt = tokenizer(prompt, return_tensors='pt')['input_ids'].cuda()
+    with torch.inference_mode():
+        output = model.generate(tokenized_prompt, 
+                            max_new_tokens=test_config.max_new_tokens, 
+                            generation_config=test_config.gen_config)
+        
+    return tokenizer.decode(output[0][len(tokenized_prompt[0]):], skip_special_tokens=True)
